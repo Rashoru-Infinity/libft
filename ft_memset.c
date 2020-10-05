450ff888b7b607300f9e0ec36d2eebb3dbe2fab6
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-06 06:52:43 +0900

    modified:   ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -12,12 +12,12 @@
 
 #include "libft.h"
 
-void *ft_memset(void *buf, int ch, size_t n)
+void	*ft_memset(void *buf, int ch, size_t n)
 {
 	unsigned char	*buf2;
 
 	buf2 = (unsigned char *)buf;
 	while (n-- > 0)
 		*(buf2++) = (unsigned char)ch;
-	return buf;
+	return (buf);
 }
