b78c003a1617a4239a7c00fa651bee0e02d2bd9c
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-06 16:29:47 +0900

    new file:   ft_strlen.c 	modified:   main.c

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 #include <stdlib.h>
 
 int main(void) {
-	char str1[] = "string1";
-	char str2[] = "string2";
-	printf("ft_diff : %d\n", ft_memcmp(str1, str2, 7));
-	printf("original diff : %d\n", memcmp(str1, str2, 7));
+	char str1[] = "st\0ring1";
+	char str2[] = "st\0ring2";
+	printf("ft_diff : %d\n", ft_memcmp(str1, str2, 8));
+	printf("original diff : %d\n", memcmp(str1, str2, 8));
 	return 0;
 }
