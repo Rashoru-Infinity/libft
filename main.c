e904d9a734934ce97572230ee6f7589b55618217
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-06 21:50:18 +0900

    modified:   main.c

--- main.c
+++ main.c
@@ -7,9 +7,9 @@
 int main(void) {
 	char dest[15] = "abc";
 	char src[] = "trucmachin";
-	printf("%zu\n", ft_strlcat(dest, src, 5));
+	printf("%zu\n", ft_strlcat(dest, src, 9));
 	printf("%s\n", dest);
-	printf("%zu\n", strlcat(dest, src, 5));
+	printf("%zu\n", strlcat(dest, src, 9));
 	printf("%s\n", dest);
 	return 0;
 }
