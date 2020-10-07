ef729076b983a4d6e89653c5a9d8997c6b00f2ff
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-07 20:24:05 +0900

    modified:   .gitignore 	modified:   Makefile 	modified:   ft_isalpha.c 	modified:   libft.h 	modified:   main.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_isalpha(int c)
+{
+	if (('A' <= (unsigned char)c && (unsigned char)c <= 'Z') ||\
+		 ('a' <= (unsigned char)c && (unsigned char)c <= 'z'))
+		return (1);
+	return (0);
+}
--- main.c
+++ main.c
@@ -3,16 +3,14 @@
 #include <string.h>
 #include <strings.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(void) {
-	char str1[] = "999999999999";
-	printf("%d\n", atoi(str1));
-	printf("%d\n", ft_atoi(str1));
-	char str2[] = "+012 34";
-	printf("%d\n", atoi(str2));
-	printf("%d\n", ft_atoi(str2));
-	char str3[] = "-\t1234";
-	printf("%d\n", atoi(str3));
-	printf("%d\n", ft_atoi(str3));
+	char c = 0;
+	for (int i = 0;i < 255;i++) {
+		//printf("%d\n", isalpha(c));
+		printf("%d\n", ft_isalpha(c));
+		++c;
+	}
 	return 0;
 }
