9a7ea9160af6c0463d0fcaa026d36b1aeb35214b
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-07 19:53:15 +0900

    new file:   ft_isalpha.c 	modified:   main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(void) {
 	char str1[] = "999999999999";
 	printf("%d\n", atoi(str1));
 	printf("%d\n", ft_atoi(str1));
-	char str2[] = "+-+1234";
+	char str2[] = "+012 34";
 	printf("%d\n", atoi(str2));
 	printf("%d\n", ft_atoi(str2));
 	char str3[] = "-\t1234";
