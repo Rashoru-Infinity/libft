f354b5e8b3163020f80df07e2110c7cdd2ccb01a
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-08 10:10:39 +0900

    modified:   main.c

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 #include <ctype.h>
 
 int main(void) {
-	for (int i = -256;i < 256;i++) {
-		printf("%d->%d\n", i, isprint(i));
+	for (int i = -999;i < 1000;i++) {
+		//printf("%d->%d\n", i, isprint(i));
 		printf("%d->%d\n", i, ft_isprint(i));
 	}
 	return 0;
