4c55776df9a771df079461013fdf69213ac739c5
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-08 21:38:48 +0900

    modified:   main.c

--- main.c
+++ main.c
@@ -6,8 +6,13 @@
 #include <ctype.h>
 
 int main(void) {
-	char *src = NULL;
-	char *dest = NULL;
-	printf("%p\n", ft_memcpy(dest, src, 1));
+	char src[] = "test basic du memccpy !";
+	char dest[22];
+	for(int i = 0;i < 22;i++)
+		printf("%c->%p\n", src[i], &src[i]);
+	char *p = memccpy(dest, src, 'm', 22);
+	printf("%c->%p\n", *p, p);
+	p = ft_memccpy(dest, src, 'm', 22);
+	printf("%c->%p\n", *p, p);
 	return 0;
 }
