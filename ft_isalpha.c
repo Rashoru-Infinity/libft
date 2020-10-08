87f7a9bdd76d956938441e481a4e4476b693f859
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-08 21:51:05 +0900

    norm:   ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -15,7 +15,7 @@
 int	ft_isalpha(int c)
 {
 	if (('A' <= (unsigned char)c && (unsigned char)c <= 'Z') ||\
-		 ('a' <= (unsigned char)c && (unsigned char)c <= 'z'))
+		('a' <= (unsigned char)c && (unsigned char)c <= 'z'))
 		return (1);
 	return (0);
 }
