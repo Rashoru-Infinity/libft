d218fc6f67880a12e9055d6b7e706f92969744ba
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-08 10:36:30 +0900

    new file:   ft_tolower.c 	modified:   main.c

--- main.c
+++ main.c
@@ -6,9 +6,9 @@
 #include <ctype.h>
 
 int main(void) {
-	for (int i = -999;i < 1000;i++) {
-		printf("%c->%c\n", i, toupper(i));
-		printf("%d->%c\n", i, ft_toupper(i));
+	for (int i = -1000;i < 1000;i++) {
+		//printf("%d->%d\n", i, toupper(i));
+		printf("%d->%d\n", i, ft_toupper(i));
 	}
 	return 0;
 }
