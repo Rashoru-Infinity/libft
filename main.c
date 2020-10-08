d34eb6c5b506766c299b60faf06e2abedefcf83e
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-08 10:13:41 +0900

    modified:   main.c

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 
 int main(void) {
 	for (int i = -999;i < 1000;i++) {
-		//printf("%d->%d\n", i, isdigit(i));
-		printf("%d->%d\n", i, ft_isdigit(i));
+		printf("%d->%d\n", i, isdigit(i));
+		//printf("%d->%d\n", i, ft_isdigit(i));
 	}
 	return 0;
 }
