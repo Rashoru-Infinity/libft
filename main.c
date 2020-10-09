26c0ec53fbd2c6a7c5d9339bd60658f1872e7877
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-09 11:08:15 +0900

    modified:   main.c

--- main.c
+++ main.c
@@ -6,20 +6,20 @@
 #include <ctype.h>
 
 int main(void) {
-	char dest1[30] = "dest part\0";
-	char dest2[30] = "dest part\0";
-	char dest3[30] = "dest part\0";
-	char dest4[30] = "dest part\0";
+	char dest1[30] = "dest part ";
+	char dest2[30] = "dest part ";
+	char dest3[30] = "dest part ";
+	char dest4[30] = "dest part ";
 	char src[] = "src part";
 	printf("-------------------------------------------\n");
-	printf("%zu\n", strlcpy(dest1, src, 16));
+	printf("%zu\n", strlcat(dest1, src, 16));
 	printf("%s\n", dest1);
-	printf("%zu\n", ft_strlcpy(dest2, src, 16));
+	printf("%zu\n", ft_strlcat(dest2, src, 16));
 	printf("%s\n", dest2);
 	printf("------------null byte test-----------------\n");
-	printf("%zu\n", strlcpy(dest3, "", 30));
+	printf("%zu\n", strlcat(dest3, "", 30));
 	printf("%s\n", dest3);
-	printf("%zu\n", ft_strlcpy(dest4, "", 30));
+	printf("%zu\n", ft_strlcat(dest4, "", 30));
 	printf("%s\n", dest4);	
 	return 0;
 }
