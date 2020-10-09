8d6c5c57544a1c3f235673e4db103c44bcc01650
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-09 09:56:29 +0900

    modified:   main.c

--- main.c
+++ main.c
@@ -8,9 +8,35 @@
 int main(void) {
 	char dest[30];
 	char src[] = "string";
+	printf("-------------------------------------------");
+	printf("%zu\n", strlcpy(dest, src, 0));
+	printf("%s\n", dest);
+	printf("%zu\n", ft_strlcpy(dest, src, 0));
+	printf("%s\n", dest);
+	printf("-------------------------------------------");
+	printf("%zu\n", strlcpy(dest, src, 6));
+	printf("%s\n", dest);
+	printf("%zu\n", ft_strlcpy(dest, src, 6));
+	printf("%s\n", dest);
+	printf("-------------------------------------------");
+	printf("%zu\n", strlcpy(dest, src, 7));
+	printf("%s\n", dest);
+	printf("%zu\n", ft_strlcpy(dest, src, 7));
+	printf("%s\n", dest);
+	printf("-------------------------------------------");
+	printf("%zu\n", strlcpy(dest, src, 8));
+	printf("%s\n", dest);
+	printf("%zu\n", ft_strlcpy(dest, src, 8));
+	printf("%s\n", dest);
+	printf("-------------------------------------------");
 	printf("%zu\n", strlcpy(dest, src, 30));
 	printf("%s\n", dest);
 	printf("%zu\n", ft_strlcpy(dest, src, 30));
 	printf("%s\n", dest);
+	printf("------------null byte test-----------------");
+	printf("%zu\n", strlcpy(dest, "", 30));
+	printf("%s\n", dest);
+	printf("%zu\n", ft_strlcpy(dest, "", 30));
+	printf("%s\n", dest);	
 	return 0;
 }
