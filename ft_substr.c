b8bdd4fb7ebabbc6ef1a9f968fd7e631838fc771
Keita Hagiwara <khagiwar@c1r16s8.42tokyo.jp>
2020-10-09 19:39:20 +0900

    modified:   ft_strdup.c 	new file:   ft_substr.c 	modified:   main.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,35 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khagiwar <khagiwar@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/08 18:34:08 by khagiwar          #+#    #+#             */
-/*   Updated: 2020/10/09 19:21:58 by khagiwar         ###   ########.fr       */
+/*   Created: 2020/10/09 19:39:13 by khagiwar          #+#    #+#             */
+/*   Updated: 2020/10/09 19:39:15 by khagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-char	*ft_strdup(const char *s)
-{
-	size_t	len;
-	char	*p;
-	size_t	offset;
-
-	len = 0;
-	while (*(s + len))
-		++len;
-	if ((p = (char *)malloc(len + 1)))
-	{
-		offset = 0;
-		while (offset < len)
-		{
-			*(p + offset) = *(char *)(s + offset);
-			++offset;
-		}
-		*(p + offset) = 0;
-	}
-	return (p);
-}
--- main.c
+++ main.c
@@ -6,22 +6,9 @@
 #include <ctype.h>
 
 int main(void) {
-	char dest1[1024] = "there is no stars in the sky\0";
-	char dest2[1024] = "there is no stars in the sky\0";
-	char dest3[1024] = "there is no stars in the sky\0";
-	char dest4[1024] = "there is no stars in the sky\0";
-	char src[] = "the cake is a lie !\0I'm hidden lol\r\n";
-	printf("destsize->%zu\n", strlen(dest1));
-	printf("srcsize->%zu\n", strlen(src));
-	printf("-------------------------------------------\n");
-	printf("%zu\n", strlcat(dest1, src, 25));
-	printf("%s\n", dest1);
-	printf("%zu\n", ft_strlcat(dest2, src, 25));
-	printf("%s\n", dest2);
-	printf("------------null byte test-----------------\n");
-	printf("%zu\n", strlcat(dest3, "", 0));
-	printf("%s\n", dest3);
-	printf("%zu\n", ft_strlcat(dest4, "", 0));
-	printf("%s\n", dest4);	
+	char *str;
+	char *tmp = "this is a normal test";
+	str = ft_strdup(tmp);
+	printf("%s\n", str);
 	return 0;
 }
