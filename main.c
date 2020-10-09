eea6da6b7cd30c2ab3079f1d28255548709b9c18
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-09 09:44:34 +0900

    modified:   main.c

--- main.c
+++ main.c
@@ -6,13 +6,11 @@
 #include <ctype.h>
 
 int main(void) {
-	char src[] = "test basic du memccpy !";
-	char dest[22];
-	for(int i = 0;i < 22;i++)
-		printf("%c->%p\n", src[i], &src[i]);
-	char *p = memccpy(dest, src, 'm', 22);
-	printf("%c->%p\n", *p, p);
-	p = ft_memccpy(dest, src, 'm', 22);
-	printf("%c->%p\n", *p, p);
+	char dest[30];
+	char src[] = "string";
+	printf("%zu\n", strlcpy(dest, src, 6));
+	printf("%s\n", dest);
+	printf("%zu\n", ft_strlcpy(dest, src, 6));
+	printf("%s\n", dest);
 	return 0;
 }
