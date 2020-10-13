eaa5b5886543444e594374ba19a7e4a8c9a43f93
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-13 22:16:03 +0900

    modified:   ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,6 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	offset1;
 	size_t	offset2;
 
+	if (!s1 || !s2)
+		return (NULL);
 	if ((str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
 	{
 		offset1 = 0;
