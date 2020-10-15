a172a0b9527031b1d69994a1bcf03decf6ef2071
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-15 22:20:26 +0900

    modified:   Makefile 	modified:   ft_lstadd_back.c 	modified:   ft_lstadd_front.c 	modified:   libft.h

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -10,4 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *p;
+
+	if (!lst)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	p = *lst;
+	while (!p->next)
+	{
+		p = p->next;
+	}
+	p = new;
+}
