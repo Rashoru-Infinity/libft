c5c1660415d6bc07074205754842920714e2b649
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-15 22:04:32 +0900

    modified:   Makefile 	modified:   ft_lstlast.c 	modified:   libft.h

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list *p;
+
+	p = lst;
+	while (p->next)
+	{
+		p = p->next;
+	}
+	return (p);
+}
