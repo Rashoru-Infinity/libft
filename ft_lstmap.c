2dfde69f34015ca4aa0bcbc131a2cbbc3d5a834c
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-16 23:52:30 +0900

    modified:   Makefile 	modified:   ft_lstmap.c 	modified:   libft.h

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*currlst;
+	void	*tmp;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	currlst = lst;
+	while (currlst)
+	{
+		tmp = f(currlst->content);
+		if (tmp)
+		{
+			(*currlst).content = tmp;
+			currlst = currlst->next;
+		}
+		else
+		{
+			while (lst)
+			{
+				currlst = lst->next;
+				ft_lstdelone(lst, del);
+				lst = currlst;
+			}
+			return (NULL);
+		}
+	}
+	return (lst);
+}
