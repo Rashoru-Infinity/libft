860474a845edc7966f1b0aac297c7bae5257b530
khagiwar <khagiwar@student.42tokyo.jp>
2020-10-23 17:08:25 +0900

    modified:   ft_putchar_fd.c

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -19,14 +19,14 @@ void	ft_putchar_fd(char c, int fd)
 	char mask1;
 	char mask2;
 
-	if (c <= 0x7f)
+	if ((unsigned char)c <= 0x7f)
 		write(fd, &c, 1);
 	else
 	{
 		mask1 = 0b00111111;
 		mask2 = 0b11000000;
 		head = (((unsigned char)c & mask2) >> 6 | mask2);
-		tail = (((unsigned char)c & mask1) | 0b10000000);
+		tail = ((unsigned char)c & mask1) | 0b10000000;
 		write(fd, &head, 1);
 		write(fd, &tail, 1);
 	}
